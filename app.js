3c2715b1d54c3e02174bdbaa7a35cd2da7a91b74
Avishkar Rangari <97297953+rangari123@users.noreply.github.com>
2023-04-05 12:43:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const submit = document.querySelector(".submit");
+const end = document.querySelector(".end")
+submit.addEventListener("click",function(){
+   end.textContent="Your account is closed you can leave this window"
+})
+
+
